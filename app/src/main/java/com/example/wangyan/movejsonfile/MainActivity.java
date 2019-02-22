434c0ba6dae489eff71d3610c30c72edec92e9e1
TommorowIsBetter <841331749@qq.com>
2019-02-22 16:18:15 +0800

    add show dialog function, it shows when the file folder does not have any files.

--- app/src/main/java/com/example/wangyan/movejsonfile/MainActivity.java
+++ app/src/main/java/com/example/wangyan/movejsonfile/MainActivity.java
@@ -14,6 +14,14 @@ import java.util.List;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import android.support.v7.app.AlertDialog;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 public class MainActivity extends AppCompatActivity {
 
     File sdCardDir = Environment.getExternalStorageDirectory();
@@ -24,12 +32,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClick(android.view.View view){
+        if(checkFile())
+        {
+            showDialog();
+            return;
+        }
         newDirectory(sdCardDir.toString(), "TestDir");//首先先建一个TestDir文件夹
         Date date = new Date();//这里目的是将当前时间转换为字符串，然后为后面建立文件夹
         DateFormat format = new SimpleDateFormat("yyyyMMddHHmmss");
         String now = format.format(date);
         moveFileFolder(sdCardDir.toString()+"/ThinkTime", now);//把原先的文件夹里面的所有
         //文件移动到新的文件夹中去
+
     }
 
     public void newDirectory(String _path, String dirName){//新建一个文件夹的函数
@@ -61,5 +75,33 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
+    }
+
+   //点击button键在界面弹出提示
+    public void showDialog(){
+        AlertDialog.Builder builder=new AlertDialog.Builder(this);
+        builder.setTitle("温馨提示");
+        builder.setMessage("文件夹为空！");
+        builder.setPositiveButton("我知道了",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+        AlertDialog dialog=builder.create();
+        dialog.show();
+
+    }
+
+    //检查指定文件夹下是否有文件
+    public boolean checkFile(){
+        String filePath = sdCardDir.toString() + "/ThinkTime";
+        File baseFile = new File(filePath);//这里可以填写文件路径，而不只是具体的某个文件
+        File[] fileList = baseFile.listFiles();//把文件下的所有文件都放到list列表中去
+        if(fileList.length == 0)
+            return true;
+        else
+            return false;
     }
 }
